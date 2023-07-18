b705beb9cc31f8b49c81068df61c8d05b50de820
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-07-18 10:37:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+//name
+let myname = "hassan";
+console.log(myname);
+//age
+const age = 18;
+console.log(age);
+//address
+var address = "RWP";
+console.log(address);
+//data type
+console.log(typeof myname);
+console.log(typeof age);
+//operators
+//+,-,*,/,%
+//addition
+let num1 = 1;
+let num2 = 9;
+console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+//logical operators
+//&&,||,!
+console.log(num1 < 3 && num2 > 70);
+console.log(num1 > 9 || num2 < 10);
